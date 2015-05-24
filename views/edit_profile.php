bf3e67973c4d9e0fbb6db3eea44ba47a70b7829c
Takkasila <takkasila@outlook.com>
2015-05-25 04:27:52 +0700

    Profile edit page and add book

--- views/edit_profile.php
+++ views/edit_profile.php
@@ -5,30 +5,55 @@ if (isset($profile)) {
             echo $error;
         }
     }
-    if ($profile->messages) {
-        foreach ($profile->messages as $message) {
-            echo $message;
-        }
-    }
-
 }
 ?>
 
-<h1>  <?php echo $_SESSION['username']; ?> </h1>
+<h2 class="content-head is-center">
+    <?php echo $profile->data["first_name"]." ".$profile->data["last_name"]; ?>
+</h2>
 
 <!-- edit profile form -->
 <form method="post" action="profile.php?uid=<?php echo $_SESSION['uid']; ?>" name="editProfileform">
 
-    <label for="editProfile_first_name">First Name</label>
-    <input id="editProfile_first_name" type="text" name="first_name" value="<?php echo $profile->data['first_name']; ?>" required />
-
-    <label for="editProfile_last_name">Last Name</label>
-    <input id="editProfile_last_name" type="text" name="last_name" value="<?php echo $profile->data['last_name']; ?>" required />
-
-    <label for="about">About you</label>
-    <textarea id="editProfile_about" name="about" > <?php echo $profile->data['about']; ?> </textarea>
+    <div style="display: block">
+        <div class="pure-u-1-4 pure-u-md-1-4 pure-u-lg-1-4">
+        </div>
+        <div class="pure-u-1-4 pure-u-md-1-4 pure-u-lg-1-4">
+            <div style="display: block; text-align: center;">
+                <label for="editProfile_first_name">First Name</label>
+            </div>
+            <div style="display: block; text-align: center;">
+                <input id="editProfile_first_name" type="text" name="first_name" value="<?php echo $profile->data['first_name']; ?>" required />
+            </div>
+        </div>
+        <div class="pure-u-1-4 pure-u-md-1-4 pure-u-lg-1-4" style="vertical-align: middle;">
+            <div style="display: block; text-align: center;">
+                <label for="editProfile_last_name">Last Name</label>
+            </div>
+            <div style="display: block; text-align: center;">
+                <input id="editProfile_last_name" type="text" name="last_name" value="<?php echo $profile->data['last_name']; ?>" required />    
+            </div>
+        </div>
+    </div>
     
-    <input type="submit"  name="updateProfile" value="UpdateProfile" />
+    <div display:block">
+        <div class="pure-u-1-1 pure-u-md-1-3 pure-u-lg-1-3">
+        </div>
+
+        <div class="pure-u-1-1 pure-u-md-1-3 pure-u-lg-1-3">
+            <label for="about" style="display:block; text-align: center;">About you</label>
+
+            <div style="display:block">
+                <textarea id="editProfile_about" name="about" rows="4" cols="70">
+                <?php echo $profile->data['about']; ?></textarea>
+            </div>
+            
+            <div style="display:block; text-align: center; margin: auto">
+                <input type="submit" name="updateProfile" value="UpdateProfile" style="display:block; text-align: center;"/>
+            </div>
+        </div>
+    </div>
+
 
 </form>
 
--- views/edit_user_book.php
+++ views/edit_user_book.php
@@ -10,71 +10,86 @@
                 echo $message;
             }
         }
-
-        //var_dump($book->data);
-       // echo json_encode($book->data);
     }
 ?>
 
 <html>
-<head>
-<meta charset="UTF-8">
-<script>
-function showResult(str) {
-  if (str.length==0) { 
-    document.getElementById("livesearch").innerHTML="";
-    document.getElementById("livesearch").style.border="0px";
-    return;
-  }
-  if (window.XMLHttpRequest) {
-    // code for IE7+, Firefox, Chrome, Opera, Safari
-    xmlhttp=new XMLHttpRequest();
-  } else {  // code for IE6, IE5
-    xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
-  }
-  xmlhttp.onreadystatechange=function() {
-    if (xmlhttp.readyState==4 && xmlhttp.status==200) {
-      document.getElementById("livesearch").innerHTML=xmlhttp.responseText;
-      document.getElementById("livesearch").style.border="1px solid #A5ACB2";
-    }
-  }
-  xmlhttp.open("GET","book.php?searchKeyword="+str,true);
-  xmlhttp.send();
-}
-</script>
-</head>
-<body>
-<h2> My books</h2>
-<?php
-    if ($book->data) {
-        foreach ($book->data as $book_data) {
-            echo "<li>" . $book_data["book_name"] . "</li>";
+  <head>
+    <meta charset="UTF-8">
+    <script>
+      function showResult(str) {
+        if (str.length==0) { 
+          document.getElementById("livesearch").innerHTML="";
+          document.getElementById("livesearch").style.border="0px";
+          return;
         }
-    }
-?>
-</br>
-<form>
-    <label>Search book by ISBN/Name</label>
-    <input type="text" size="30" onkeyup="showResult(this.value)">
-    <div id="livesearch"></div>
-</form>
+        if (window.XMLHttpRequest) {
+          // code for IE7+, Firefox, Chrome, Opera, Safari
+          xmlhttp=new XMLHttpRequest();
+        } else {  // code for IE6, IE5
+          xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+        }
+        xmlhttp.onreadystatechange=function() {
+          if (xmlhttp.readyState==4 && xmlhttp.status==200) {
+            document.getElementById("livesearch").innerHTML=xmlhttp.responseText;
+            document.getElementById("livesearch").style.border="1px solid #FFFFFF";
+          }
+        }
+        xmlhttp.open("GET","book.php?searchKeyword="+str,true);
+        xmlhttp.send();
+      }
+    </script>
+  </head>
+  <body>
+    <div class="pure-u-1 pure-u-md-1-1 pure-u-lg-1-1">
+      <div class="pure-u-1 pure-u-md-1-1 pure-u-lg-1-1" style="text-align: center;">
+        <h2 style="color:white">Search book by ISBN/Name</h2>
+      </div>
+      <div class="pure-u-1 pure-u-md-1-3 pure-u-lg-1-3">
+      </div>
+      <div class="pure-u-1 pure-u-md-1-3 pure-u-lg-1-3">
+        <form class="pure-form">
+          <input type="text" class="pure-input-rounded" onkeyup="showResult(this.value)">
+          <div style="text-align: center;">
+            <button type="submit" class="pure-button">Search</button>
+          </div>
+          <div id="livesearch"></div>
+        </form>
+      </div>
+    </div>
 
-<form method="post" action="book.php" name="createBookform">
+    <div class="pure-u-1 pure-u-md-1-1 pure-u-lg-1-1" style="margin-top:5em">
+      <div class="pure-u-1 pure-u-md-1-1 pure-u-lg-1-1">
+        <h2 style="color:orange; text-align: center;">Create new book</h2>
+      </div>
+      <div class="pure-u-1 pure-u-md-1-1 pure-u-lg-1-1">
+        <form class="pure-form pure-form-stacked" method="post" action="book.php" name="createBookform">
+          <fieldset>
+              <div class="pure-g">
+                  <div class="pure-u-1 pure-u-md-1-3">
+                      <label for="first-name" style="color:white">ISBN</label>
+                      <input class="pure-u-23-24" id="createBook_ISBN" type="text" pattern="[0-9]{2,14}" name="book_isbn" required />
+                  </div>
 
-    <label for="createBook_ISBN">ISBN</label>
-    <input id="createBook_ISBN" type="text" pattern="[0-9]{2,14}" name="book_isbn" required />
+                  <div class="pure-u-1 pure-u-md-1-3">
+                      <label for="last-name" style="color:white">Book Name</label>
+                      <input class="pure-u-23-24" id="createBook_book_name" type="text" name="book_name" required />
+                  </div>
 
-    <label for="createBook_book_name">Book Name</label>
-    <input id="createBook_book_name" type="text" name="book_name" required />
-
-    <label for="creatBook_book_description">Book Description</label>
-    <input id="createBook_book_description" type="text" name="book_description" required />
-
-
-    <input type="submit"  name="createBook" value="CreateBook" />
-
-</form>
-
-
-</body>
+                  <div class="pure-u-1 pure-u-md-1-3">
+                      <label for="email" style="color:white">Book Description</label>
+                      <input class="pure-u-23-24" id="createBook_book_description" type="text" name="book_description" required />
+                  </div>
+                  <div class="pure-u-1 pure-u-md-1-3">
+                  </div>
+                  <div class="pure-u-1 pure-u-md-1-3" style="text-align:center">
+                    <button type="submit" name="createBook" value="CreateBook" class="pure-button pure-button-primary">Create</button>
+                  </div>
+              </div>
+            </fieldset>
+        </form>
+        <!-- End of form -->
+      </div>
+    </div>
+  </body>
 </html>
